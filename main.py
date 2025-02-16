3098bd1ca0d04968403d9e4e5452a2f7531ed5e6
coachnrm <nuttapong_coach@hotmail.com>
2025-02-16 07:49:27 +0700

    add predict model

--- main.py
+++ main.py
@@ -35,23 +35,39 @@ df['CommonCold'].value_counts() # นับข้อมูลของ column �
 from sklearn.feature_selection import chi2
 
 # Prepare features and target variable
-X = df[['Head','Neck']]
+X = df[['Head','Nose','Neck','Fever']]
 y = df['CommonCold']
 
 from sklearn.model_selection import train_test_split
-from sklearn import svm
+from sklearn.ensemble import RandomForestClassifier
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
 
-modelSVM = svm.SVC(kernel='linear')
-modelSVM.fit(X_train, y_train)
+modelRF = RandomForestClassifier(n_estimators=100, random_state=42, max_depth=5)
+modelRF.fit(X_train, y_train)
 
-
-y_pred = modelSVM.predict(X_test)
+y_pred = modelRF.predict(X_test)
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
-print(confusion_matrix(y_test, y_pred))
-print(classification_report(y_test, y_pred))
-print(accuracy_score(y_test, y_pred))
+# print(confusion_matrix(y_test, y_pred))
+# print(classification_report(y_test, y_pred))
+# print(accuracy_score(y_test, y_pred))
+
+import pickle
+filename = 'common-cold.sav'
+pickle.dump(modelRF,open(filename, 'wb'))
+
+load_model = pickle.load(open(filename,'rb'))
+# print(load_model.predict([[1, 1, 1, 1]]))
+
+
+# Define column names based on the training data
+feature_names = ['Head', 'Nose', 'Neck', 'Fever']
+
+# Convert input to a DataFrame
+input_data = pd.DataFrame([[1, 1, 1, 0]], columns=feature_names)
+
+# Make prediction
+print(load_model.predict(input_data))
 
 
 
